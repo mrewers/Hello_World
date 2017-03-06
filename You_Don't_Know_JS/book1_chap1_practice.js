b979ecf182a26ec16f4b33a4ce379c039da6c4d8
mrewers <marekrewers@gmail.com>
2017-03-06 17:04:43 -0500

    Adding directory within which to store 'You Don't Know JS' practice work

---
+++ You_Don't_Know_JS/book1_chap1_practice.js
@@ -0,0 +1,3 @@
+a = 21; // Defines variable a
+b = a * 2; // Defines variable b as an expression of a multiplied by two
+console.log(b); // Prints variable b to the console resulting in the output '42'
