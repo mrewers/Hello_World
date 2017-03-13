d691b0d4f99fdcc9fd749017c3a11f6ebb00e0e0
mrewers <marekrewers@gmail.com>
2017-03-13 16:45:50 -0400

    Additional practice examples added to You Don't Know JS

--- You_Don't_Know_JS/book1_chap1_practice.js
+++ You_Don't_Know_JS/book1_chap1_practice.js
@@ -1,3 +1,23 @@
+//Variables
+
 a = 21; // Defines variable a
 b = a * 2; // Defines variable b as an expression of a multiplied by two
 console.log(b); // Prints variable b to the console resulting in the output '42'
+
+//INPUTS
+
+age = prompt( "Please tell me your age:" ); // Provides user with input prompt
+console.log( age ); // Print's their response
+
+// COERCION
+// Method by which a string is converted into a number
+
+var a = "42";
+var b = Number( a );
+
+console.log( a );   // Outputs "42" - string
+console.log( b );   // Outputs 42 - number
+
+// Implicit coercion
+
+"99.99" == 99.99 // JS will implicitly coerce left-handed string to it's number equivalent
