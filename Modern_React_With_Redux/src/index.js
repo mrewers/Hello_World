75c8a8e1f65894de448177ab229215c6d8377cfa
Marek <marekrewers@gmail.com>
2017-09-16 00:48:09 -0400

    Initializing (my version of the) project

--- Modern_React_With_Redux/src/index.js
+++ Modern_React_With_Redux/src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// Create a new component to produce some HTML.
+const App = () => {
+  return <h1>Masternode Tracker</h1>;
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Insert the generated HTML in the DOM.
+ReactDOM.render(<App />, document.querySelector('.container'));
