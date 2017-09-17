7b96194a8c7a5f582e4cb400bfd391f3f1021e05
Marek <marekrewers@gmail.com>
2017-09-17 16:29:43 -0400

    Completed video player app

--- Modern_React_With_Redux/src/index.js
+++ Modern_React_With_Redux/src/index.js
@@ -1,20 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import _ from 'lodash';
+import YTSearch from 'youtube-api-search';
 
-import YouTube from '../keys.js'
-import SearchBar from '../components/search_bar.js';
+import YouTubeKey from '../keys.js'
+import SearchBar from './components/search_bar.js';
+import VideoList from './components/video_list.js';
+import VideoDetail from './components/video_detail.js';
 
-const API_KEY = <YouTube />;
+const API_KEY = `${YouTubeKey}`;
 
-// Create a new component to produce some HTML.
-const App = () => {
-  // return <h1>Masternode Tracker</h1>;
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo:null
+    };
+
+    this.videoSearch('chaincoin');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
+    return (
+      <div>
+        <h1>Masternode Tracker</h1>
+        <SearchBar onSearchTermChange={ videoSearch }/>
+        <VideoDetail video={ this.state.selectedVideo }/>
+        <VideoList
+          onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
+          videos={ this.state.videos } />
+      </div>
+    );
+  }
 }
 
-// Insert the generated HTML in the DOM.
 ReactDOM.render(<App />, document.querySelector('.container'));
