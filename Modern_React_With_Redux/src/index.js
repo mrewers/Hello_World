59ed2ace6d032dd658cb38612046d2cad7434a7c
Marek <marekrewers@gmail.com>
2017-09-16 23:17:29 -0400

    Adding basic search bar

--- Modern_React_With_Redux/src/index.js
+++ Modern_React_With_Redux/src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import YouTube from '../keys.js'
+import SearchBar from '../components/search_bar.js';
+
+const API_KEY = <YouTube />;
+
 // Create a new component to produce some HTML.
 const App = () => {
-  return <h1>Masternode Tracker</h1>;
+  // return <h1>Masternode Tracker</h1>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Insert the generated HTML in the DOM.
