0f283473c7565510b4d4d7ee240071c656d74b05
Marek Rewers <marekrewers@gmail.com>
2018-04-18 23:29:38 -0400

    Add display and toggle all buttons with corresponding event listeners

--- Watch_And_Code/Practical_JS/script.js
+++ Watch_And_Code/Practical_JS/script.js
@@ -57,3 +57,14 @@ var todoList = {
     this.displayTodos();
   }
 }
+
+var displayTodosButton = document.getElementById('displayTodosButton');
+var toggleAllButton = document.getElementById('toggleAllButton');
+
+displayTodosButton.addEventListener('click', function() {
+  todoList.displayTodos();
+});
+
+toggleAllButton.addEventListener('click', function() {
+  todoList.toggleAll();
+});
