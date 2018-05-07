833f2a2d14543c5a78466ed54263b7475257ba6a
Marek Rewers <marekrewers@gmail.com>
2018-05-07 00:08:53 -0400

    Refactor initial app into JSX

--- Frontend_Masters/Complete_Intro_to_React/js/ClientApp.jsx
+++ Frontend_Masters/Complete_Intro_to_React/js/ClientApp.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const ce = React.createElement;
-
 const MyTitle = function MyTitle(props) {
-  return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
-};
-
-const MyFirstComponent = function MyFirstComponent() {
-  return ce(
-    'div',
-    { id: 'title-list' },
-    ce(MyTitle, { title: 'The Wire', color: 'peru' }),
-    ce(MyTitle, { title: 'Game of Thrones', color: 'papayawhip' }),
-    ce(MyTitle, { title: 'Better Call Saul', color: 'lime' })
+  return (
+    <div>
+      <h1 style={{ color: props.color }}>{props.title}</h1>
+    </div>
   );
 };
 
-render(ce(MyFirstComponent), document.getElementById('app'));
+const MyFirstComponent = function MyFirstComponent() {
+  return (
+    <div id="title-list">
+      <MyTitle title="The Wire" color="peru" />
+      <MyTitle title="Game of Thrones" color="papayawhip" />
+      <MyTitle title="Better Call Saul" color="lime" />
+    </div>
+  );
+};
+
+render(<MyFirstComponent />);
