770dd3c094678efd887ee8f4526c6cdc67fc0e93
Marek Rewers <marekrewers@gmail.com>
2018-05-08 00:17:00 -0400

    Add styled component

--- Frontend_Masters/Complete_Intro_to_React/js/Search.jsx
+++ Frontend_Masters/Complete_Intro_to_React/js/Search.jsx
@@ -4,15 +4,17 @@ import preload from '../data.json';
 
 const Search = () => (
   <div className="search">
-    {preload.shows.map(show => (
-      <ShowCard
-        key={show.imdbID}
-        poster={show.poster}
-        title={show.title}
-        year={show.year}
-        description={show.description}
-      />
-    ))}
+    <div>
+      {preload.shows.map(show => (
+        <ShowCard
+          key={show.imdbID}
+          poster={show.poster}
+          title={show.title}
+          year={show.year}
+          description={show.description}
+        />
+      ))}
+    </div>
   </div>
 );
 
