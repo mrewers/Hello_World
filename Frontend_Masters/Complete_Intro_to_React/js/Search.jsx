e77daddfa22a540b8265b33540a2f2b8999263c5
Marek Rewers <marekrewers@gmail.com>
2018-05-08 00:03:51 -0400

    Create show card component

--- Frontend_Masters/Complete_Intro_to_React/js/Search.jsx
+++ Frontend_Masters/Complete_Intro_to_React/js/Search.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import ShowCard from './ShowCard';
+import preload from '../data.json';
 
-const Search = () => <h1>Search</h1>;
+const Search = () => (
+  <div className="search">
+    {preload.shows.map(show => (
+      <ShowCard
+        key={show.imdbID}
+        poster={show.poster}
+        title={show.title}
+        year={show.year}
+        description={show.description}
+      />
+    ))}
+  </div>
+);
 
 export default Search;
