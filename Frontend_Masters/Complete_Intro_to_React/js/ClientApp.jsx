c2b9baeb5b3842f7911e2f801867e6d2a2556cf9
Marek Rewers <marekrewers@gmail.com>
2018-05-07 22:51:38 -0400

    Rewrite app jsx and add link to stylesheet

--- Frontend_Masters/Complete_Intro_to_React/js/ClientApp.jsx
+++ Frontend_Masters/Complete_Intro_to_React/js/ClientApp.jsx
@@ -1,22 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const MyTitle = function MyTitle(props) {
-  return (
-    <div>
-      <h1 style={{ color: props.color }}>{props.title}</h1>
+const App = () => (
+  <div className="app">
+    <div className="landing">
+      <h1>Video App</h1>
+      <input type="text" placeholder="Search" />
+      <a>or Browse All</a>
     </div>
-  );
-};
+  </div>
+);
 
-const MyFirstComponent = function MyFirstComponent() {
-  return (
-    <div id="title-list">
-      <MyTitle title="The Wire" color="peru" />
-      <MyTitle title="Game of Thrones" color="papayawhip" />
-      <MyTitle title="Better Call Saul" color="lime" />
-    </div>
-  );
-};
-
-render(<MyFirstComponent />);
+render(<App />, document.getElementById('app'));
