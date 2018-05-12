f36f9b8204127275fe0d947a801d350621a7ab2a
Marek Rewers <marekrewers@gmail.com>
2018-05-11 23:03:43 -0400

    Connect search, refactor

--- Frontend_Masters/Complete_Intro_to_React/js/Search.jsx
+++ Frontend_Masters/Complete_Intro_to_React/js/Search.jsx
@@ -2,11 +2,14 @@
 
 import React, { Component } from 'react';
 import ShowCard from './ShowCard';
-import preload from '../data.json';
+import Header from './Header';
 
 class Search extends Component {
   state = {
     searchTerm: ''
+  };
+  props: {
+    shows: Array<Show>
   };
   handleSearchTermChange = (event: SyntheticKeyboardEvent & { target: HTMLInputElement }) => {
     this.setState({ searchTerm: event.target.value });
@@ -14,30 +17,14 @@ class Search extends Component {
   render() {
     return (
       <div className="search">
-        <header>
-          <h1>Video</h1>
-          <input
-            value={this.state.searchTerm}
-            type="text"
-            onChange={this.handleSearchTermChange}
-            placeholder="Search"
-          />
-        </header>
+        <Header showSearch handleSearchTermChange={this.handleSearchTermChange} searchTerm={this.state.searchTerm} />
         <div>
-          {preload.shows
+          {this.props.shows
             .filter(
               show =>
                 `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0
             )
-            .map(show => (
-              <ShowCard
-                key={show.imdbID}
-                poster={show.poster}
-                title={show.title}
-                year={show.year}
-                description={show.description}
-              />
-            ))}
+            .map(show => <ShowCard key={show.imdbID} {...show} />)}
         </div>
       </div>
     );
