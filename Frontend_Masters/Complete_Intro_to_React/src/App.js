d5c2ec605d612fcd26d7d835ec37e475d6c5e360
Marek Rewers <mrewers@users.noreply.github.com>
2019-02-12 23:05:12 -0500

    Add pet component with props

--- Frontend_Masters/Complete_Intro_to_React/src/App.js
+++ Frontend_Masters/Complete_Intro_to_React/src/App.js
@@ -1,8 +1,32 @@
+const Pet = ( props ) => {
+  return React.createElement(
+    'div', {}, [
+      React.createElement( 'h1', {}, props.name ),
+      React.createElement( 'h2', {}, props.animal ),
+      React.createElement( 'h2', {}, props.breed ),
+  ] );
+}
+
 const App = () => {
   return React.createElement(
-    'div', {},
-    React.createElement( 'h1', {}, 'Adopt Me')
-  )
+    'div', {}, [
+      React.createElement( 'h1', {}, 'Adopt Me'),
+      React.createElement( Pet, {
+        name: 'Fido',
+        animal: 'dog',
+        breed: 'bloodhound'
+      } ),
+      React.createElement( Pet, {
+        name: 'Izzard',
+        animal: 'lizard',
+        breed: 'iguana'
+      } ),
+      React.createElement( Pet, {
+        name: 'Cluck',
+        animal: 'bird',
+        breed: 'chicken'
+      } )
+  ] );
 }
 
 ReactDOM.render( React.createElement(App), document.getElementById('root'))
