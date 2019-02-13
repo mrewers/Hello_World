b6dea11d96476a8442d931efcf207e7be69a8271
Marek Rewers <mrewers@users.noreply.github.com>
2019-02-13 17:52:50 -0500

    Convert to JSX

--- Frontend_Masters/Complete_Intro_to_React/src/App.js
+++ Frontend_Masters/Complete_Intro_to_React/src/App.js
@@ -3,30 +3,16 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 class App extends React.Component {
-  handleTitleClick() {
-    alert("You clicked on the title");
-  }
-
   render() {
-    return React.createElement("div", {}, [
-      React.createElement("h1", { onClick: this.handleTitleClick }, "Adopt Me"),
-      React.createElement(Pet, {
-        name: "Fido",
-        animal: "dog",
-        breed: "bloodhound"
-      }),
-      React.createElement(Pet, {
-        name: "Izzard",
-        animal: "lizard",
-        breed: "iguana"
-      }),
-      React.createElement(Pet, {
-        name: "Cluck",
-        animal: "bird",
-        breed: "chicken"
-      })
-    ]);
+    return (
+      <div>
+        <h1>Adopt Me</h1>
+        <Pet name="Fido" animal="dog" breed="bloodhound" />
+        <Pet name="Izzard" animal="lizard" breed="iguana" />
+        <Pet name="Cluck" animal="bird" breed="chicken" />
+      </div>
+    );
   }
 }
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
