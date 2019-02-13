e9d9e1e8a883bbdc63282a8a1f069c06baee7e3d
Marek Rewers <mrewers@users.noreply.github.com>
2019-02-12 23:10:33 -0500

    Convert App component into a class

--- Frontend_Masters/Complete_Intro_to_React/src/App.js
+++ Frontend_Masters/Complete_Intro_to_React/src/App.js
@@ -7,26 +7,33 @@ const Pet = ( props ) => {
   ] );
 }
 
-const App = () => {
-  return React.createElement(
-    'div', {}, [
-      React.createElement( 'h1', {}, 'Adopt Me'),
-      React.createElement( Pet, {
-        name: 'Fido',
-        animal: 'dog',
-        breed: 'bloodhound'
-      } ),
-      React.createElement( Pet, {
-        name: 'Izzard',
-        animal: 'lizard',
-        breed: 'iguana'
-      } ),
-      React.createElement( Pet, {
-        name: 'Cluck',
-        animal: 'bird',
-        breed: 'chicken'
-      } )
-  ] );
+class App extends React.Component {
+  handleTitleClick() {
+    alert( 'You clicked on the title' );
+  }
+  
+  render() {
+    return React.createElement(
+      'div', {}, [
+        React.createElement( 'h1', { onClick: this.handleTitleClick }, 'Adopt Me'),
+        React.createElement( Pet, {
+          name: 'Fido',
+          animal: 'dog',
+          breed: 'bloodhound'
+        } ),
+        React.createElement( Pet, {
+          name: 'Izzard',
+          animal: 'lizard',
+          breed: 'iguana'
+        } ),
+        React.createElement( Pet, {
+          name: 'Cluck',
+          animal: 'bird',
+          breed: 'chicken'
+        } )
+      ]
+    );
+  }
 }
 
 ReactDOM.render( React.createElement(App), document.getElementById('root'))
