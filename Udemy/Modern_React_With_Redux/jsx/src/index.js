639c9bdefe23aabfa022119434ee0fa08b18f22c
Marek Rewers <mrewers@users.noreply.github.com>
2019-02-27 22:33:12 -0500

    JSX walkthrough

--- Udemy/Modern_React_With_Redux/jsx/src/index.js
+++ Udemy/Modern_React_With_Redux/jsx/src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+const getButtonText = () => (
+  'Click on me!'
+)
+
 const App = () => (
-  <div>Hello World</div>
+  <div>
+    <label className="label" htmlFor="name">Enter name:</label>
+    <input id="name" type="text" />
+    <button style={{ backgroundColor: 'blue', color: 'white' }}>
+      {getButtonText()}
+    </button>
+  </div>
 )
 
 ReactDom.render(
