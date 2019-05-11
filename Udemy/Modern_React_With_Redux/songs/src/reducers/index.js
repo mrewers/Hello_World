d83b6b546f2ce0635cd155c9d8b122723f6d8cee
Marek Rewers <mrewers@users.noreply.github.com>
2019-05-11 15:58:35 -0400

    Create inital songs app to test redux

---
+++ Udemy/Modern_React_With_Redux/songs/src/reducers/index.js
@@ -0,0 +1,21 @@
+import { combineReducers } from 'redux';
+
+const songsReducer = () => {
+  return [
+    { title: 'Test', duration: '4:05' },
+    { title: 'Test 2', duration: '3:20'}
+  ]
+}
+
+const selectedSongReducer = (selectedSong=null, action) => {
+  if (action.type === 'SOMG_SELECTED') {
+    return action.payload;
+  }
+
+  return selectedSong;
+}
+
+export default combineReducers( {
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+} );
