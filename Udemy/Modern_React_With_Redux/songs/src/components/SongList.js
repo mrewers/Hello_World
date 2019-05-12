db500ac6e82b08e5d6f62d88672fca87cd5fbee9
Marek Rewers <mrewers@users.noreply.github.com>
2019-05-12 15:43:33 -0400

    Render out songs list and connect action creator

--- Udemy/Modern_React_With_Redux/songs/src/components/SongList.js
+++ Udemy/Modern_React_With_Redux/songs/src/components/SongList.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectSong } from '../actions';
 
 class SongList extends Component {
+  renderList() {
+    const { selectSong, songs } = this.props;
+
+    return songs.map( song => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button
+              className="ui button primary"
+              onClick={ () => selectSong(song) }
+            >
+              Select
+            </button>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+      )
+    });
+  }
+  
   render() {
-    console.log(this.props);
     return (
-      <div>SongList</div>
+      <div className="ui divided list">{this.renderList()}</div>
     )
   }
 }
@@ -18,4 +38,6 @@ const mapStateToProps = state => {
   };
 }
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, {
+  selectSong
+})(SongList);
