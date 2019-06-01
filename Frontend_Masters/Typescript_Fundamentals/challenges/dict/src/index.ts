20edb18c9898f4c90c731f202ac7018f8ea0f16f
Marek Rewers <mrewers@users.noreply.github.com>
2019-06-01 10:19:39 -0400

    Go through generics

--- Frontend_Masters/Typescript_Fundamentals/challenges/dict/src/index.ts
+++ Frontend_Masters/Typescript_Fundamentals/challenges/dict/src/index.ts
@@ -1,7 +1,20 @@
-export type Dict = {};
+export type Dict<T> = {
+  [k: string]: T | undefined;
+};
 
 // Array.prototype.map, but for Dict
-export function mapDict() {}
+export function mapDict<T, S>(
+  dict: Dict<T>,
+  fn: (arg: T, idx: number) => S ): Dict<S> {
+    const out: Dict<S> = {};
+    Object.keys(dict).forEach((val, idx) => {
+      const thisItem = dict[val];
+      if (typeof thisItem !== 'undefined') {
+        out[val] = fn( thisItem, idx)
+      }
+    })
+    return out;
+  }
 
 // Array.prototype.reduce, but for Dict
-export function reduceDict() {}
+export function reduceDict<T>(dict: Dict<T>) {}
