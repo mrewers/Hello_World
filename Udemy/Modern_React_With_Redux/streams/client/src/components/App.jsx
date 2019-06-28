9db2162bf46f1faaa2681cdb42c9dd14eec1a38f
Marek Rewers <mrewers@users.noreply.github.com>
2019-06-27 22:31:38 -0400

    Scaffold route components

--- Udemy/Modern_React_With_Redux/streams/client/src/components/App.jsx
+++ Udemy/Modern_React_With_Redux/streams/client/src/components/App.jsx
@@ -1,28 +1,21 @@
 import React from "react";
 import { BrowserRouter, Link, Route } from "react-router-dom";
 
-const PageOne = () => (
-  <div>
-    Page One
-    <br />
-    <Link to="/2">Navigate to Page 2</Link>
-  </div>
-);
-
-const PageTwo = () => (
-  <div>
-    Page Two
-    <br />
-    <Link to="/">Navigate Home</Link>
-  </div>
-);
+import StreamList from "./streams/StreamList";
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamShow from "./streams/StreamShow";
 
 const App = () => (
   <div>
     <BrowserRouter>
       <div>
-        <Route path="/" exact component={PageOne} />
-        <Route path="/2" component={PageTwo} />
+        <Route path="/" exact component={StreamList} />
+        <Route path="/streams/new" exact component={StreamCreate} />
+        <Route path="/streams/edit" exact component={StreamEdit} />
+        <Route path="/streams/delete" exact component={StreamDelete} />
+        <Route path="/streams/show" exact component={StreamShow} />
       </div>
     </BrowserRouter>
   </div>
