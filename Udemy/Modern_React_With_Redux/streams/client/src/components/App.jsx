732455a9532de60cce3f56a7d06fbc9de9a7a950
Marek Rewers <mrewers@users.noreply.github.com>
2019-06-27 22:08:12 -0400

    Create basic routing example

--- Udemy/Modern_React_With_Redux/streams/client/src/components/App.jsx
+++ Udemy/Modern_React_With_Redux/streams/client/src/components/App.jsx
@@ -1,8 +1,31 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Link, Route } from "react-router-dom";
 
-const App = () => {
-  return <div>App</div>;
-};
+const PageOne = () => (
+  <div>
+    Page One
+    <br />
+    <Link to="/2">Navigate to Page 2</Link>
+  </div>
+);
+
+const PageTwo = () => (
+  <div>
+    Page Two
+    <br />
+    <Link to="/">Navigate Home</Link>
+  </div>
+);
+
+const App = () => (
+  <div>
+    <BrowserRouter>
+      <div>
+        <Route path="/" exact component={PageOne} />
+        <Route path="/2" component={PageTwo} />
+      </div>
+    </BrowserRouter>
+  </div>
+);
 
 export default App;
