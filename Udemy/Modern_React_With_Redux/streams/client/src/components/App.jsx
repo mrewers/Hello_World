a84b69135cea9b58c3b068abc01cdbcbcbf7ae35
Marek Rewers <mrewers@users.noreply.github.com>
2019-06-30 17:06:10 -0400

    Switch from BrowserRouter to Router

--- Udemy/Modern_React_With_Redux/streams/client/src/components/App.jsx
+++ Udemy/Modern_React_With_Redux/streams/client/src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 
 import Header from "./Header";
 import StreamList from "./streams/StreamList";
@@ -7,10 +7,11 @@ import StreamCreate from "./streams/StreamCreate";
 import StreamEdit from "./streams/StreamEdit";
 import StreamDelete from "./streams/StreamDelete";
 import StreamShow from "./streams/StreamShow";
+import history from "../history";
 
 const App = () => (
   <div className="ui container">
-    <BrowserRouter>
+    <Router history={history}>
       <Header />
       <div>
         <Route path="/" exact component={StreamList} />
@@ -19,7 +20,7 @@ const App = () => (
         <Route path="/streams/delete" exact component={StreamDelete} />
         <Route path="/streams/show" exact component={StreamShow} />
       </div>
-    </BrowserRouter>
+    </Router>
   </div>
 );
 
