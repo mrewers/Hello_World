8366606aff1e1a10e454b9608ca3270d361d31fc
Marek Rewers <mrewers@users.noreply.github.com>
2019-06-30 18:12:06 -0400

    Set up stream edit component

--- Udemy/Modern_React_With_Redux/streams/client/src/components/streams/StreamEdit.jsx
+++ Udemy/Modern_React_With_Redux/streams/client/src/components/streams/StreamEdit.jsx
@@ -1,5 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-const StreamEdit = () => <div>StreamEdit</div>;
+import StreamForm from "./StreamForm";
+import { fetchStream, editStream } from "../../actions";
 
-export default StreamEdit;
+class StreamEdit extends Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  onSubmit = formValues => {
+    this.props.editStream(this.props.match.params.id, { ...formValues });
+  };
+
+  render() {
+    const { stream } = this.props;
+    if (!stream) {
+      return <div>Loading...</div>;
+    }
+    return (
+      <div>
+        <h3>Edit a Stream</h3>
+        <StreamForm
+          initialValues={{
+            title: stream.title,
+            description: stream.description
+          }}
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.stream[ownProps.match.params.id] };
+};
+
+export default connect(
+  mapStateToProps,
+  { fetchStream, editStream }
+)(StreamEdit);
