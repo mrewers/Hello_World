c98ea60ade0cecc0b984e0c8c84aa74d67811642
Marek Rewers <mrewers@users.noreply.github.com>
2019-07-01 01:10:29 -0400

    Refactor with hooks

--- Udemy/Modern_React_With_Redux/seasons/src/index.js
+++ Udemy/Modern_React_With_Redux/seasons/src/index.js
@@ -1,48 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
 
-import Loader from './Components/Loader';
-import SeasonDisplay from './Components/SeasonDisplay/SeasonDisplay';
+import Loader from "./Components/Loader";
+import SeasonDisplay from "./Components/SeasonDisplay/SeasonDisplay";
+import useLocation from "./utils/useLocation";
 
-class App extends React.Component {
-  state = {
-    errorMessage: '',
-    lat: null
+const App = () => {
+  const [lat, errorMessage] = useLocation();
+
+  let content;
+  if (errorMessage) {
+    content = <div>Error: {errorMessage}</div>;
+  } else if (lat) {
+    content = <SeasonDisplay latitude={lat} />;
+  } else {
+    content = <Loader message="Please accept location request" />;
   }
 
+  return <div className="border red">{content}</div>;
+};
 
-  componentDidMount() {
-    window.navigator.geolocation.getCurrentPosition(
-      ( position ) => this.setState( { lat: position.coords.latitude } ),
-      ( err ) => this.setState( { errorMessage: err.message } )
-    );
-  }
-  
-  renderContent = ( errorMessage, lat ) => {
-    if ( errorMessage && !lat ) {
-      return ( <div>Error: { errorMessage }</div> );
-    }
-    
-    if ( !errorMessage && lat ) {
-      return <SeasonDisplay latitude={ lat } />;
-    }
-
-    return <Loader message="Please accept location request" />
-  }
-
-  render() {
-    const { errorMessage, lat } = this.state
-
-    return (
-      <div className="border red">
-        {this.renderContent( errorMessage, lat )}
-      </div>
-    )
-  }
-}
-
-ReactDOM.render(
-  <App />,
-  document.querySelector('#root')
-)
-
+ReactDOM.render(<App />, document.querySelector("#root"));
