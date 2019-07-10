87713d40fa0771377af0828edff1de4adddf9c45
Marek Rewers <mrewers@users.noreply.github.com>
2019-07-09 22:05:05 -0400

    Complete modules exercise

---
+++ Frontend_Masters/Intro-to-Node/exercises/modules/node/api.js
@@ -0,0 +1,18 @@
+const data = require("./data");
+
+module.exports = {
+  getUserById: (id, cb) => {
+    // simulate API call
+    setTimeout(() => {
+      const user = data.users.find(user => user.id === id);
+      cb(user);
+    }, 150);
+  },
+  getPostsForUser: (userId, cb) => {
+    // simulate API call
+    setTimeout(() => {
+      const posts = data.posts.filter(post => post.createdBy === userId);
+      cb(posts);
+    }, 150);
+  }
+};
