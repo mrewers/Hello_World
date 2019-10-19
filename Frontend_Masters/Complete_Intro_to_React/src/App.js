562315053f1043e658b6a650031a68efbab78ccd
Marek Rewers <mrewers@users.noreply.github.com>
2019-10-19 17:23:42 -0400

    Break out into seperate components

--- Frontend_Masters/Complete_Intro_to_React/src/App.js
+++ Frontend_Masters/Complete_Intro_to_React/src/App.js
@@ -1,22 +1,11 @@
 import React from "react";
 import { render } from "react-dom";
-import { Pet } from "./Pet";
+
+import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese"
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Parrot"
-    }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" })
-  ]);
+  return <SearchParams />;
 };
 
 render(<App />, document.getElementById("root"));
