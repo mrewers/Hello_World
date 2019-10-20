45e91ddf210ff889eb030ba3fe728b6b90cb7716
Marek Rewers <mrewers@users.noreply.github.com>
2019-10-20 12:22:12 -0400

    Add routing

--- Frontend_Masters/Complete_Intro_to_React/src/App.js
+++ Frontend_Masters/Complete_Intro_to_React/src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import { render } from "react-dom";
+import { Link, Router } from "@reach/router";
 
 import Pet from "./Pet";
+import Details from "./Details";
 import SearchParams from "./SearchParams";
 
 const App = () => {
-  return <SearchParams />;
+  return (
+    <StrictMode>
+      <div>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
+      </div>
+    </StrictMode>
+  );
 };
 
 render(<App />, document.getElementById("root"));
---
+++ Frontend_Masters/Complete_Intro_to_React/src/Details.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Details = () => {
+  return <div>Test</div>;
+};
+
+export default Details;
