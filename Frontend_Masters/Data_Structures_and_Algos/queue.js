78157f46ade176f3d20136d0c87d2493f65cd8ae
Marek Rewers <mrewers@users.noreply.github.com>
2020-04-19 23:19:18 -0400

    Add remaining FEM courses in progress

---
+++ Frontend_Masters/Data_Structures_and_Algos/queue.js
@@ -0,0 +1,19 @@
+function Queue(capacity) {
+  // implement me...
+}
+
+Queue.prototype.enqueue = function(value) {
+  // implement me...
+};
+
+Queue.prototype.dequeue = function() {
+  // implement me...
+};
+
+Queue.prototype.peek = function() {
+  // implement me...
+};
+
+Queue.prototype.count = function() {
+  // implement me...
+};
--- Frontend_Masters/Hard_Parts_of_Servers_and_Node/server.js
+++ Frontend_Masters/Hard_Parts_of_Servers_and_Node/server.js
@@ -1,32 +1,26 @@
-const http = require('http');
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
 
-function doOnRequest(request, response){
+function doOnRequest(request, response) {
   // Send back a message saying "Welcome to Twitter"
   // code here...
-  response.end("yo")  
-  if (request.method === 'GET' && request.url === '/') {
+  response.end("Welcome to Twitter");
+  if (request.method === "GET" && request.url === "/") {
     // read the index.html file and send it back to the client
     // code here...
-    fs.createReadStream()
-
-  }
-  else if (request.method === 'POST' && request.url === '/sayHi') {
+    fs.readFileSync("./index.html");
+    fs.createReadStream();
+  } else if (request.method === "POST" && request.url === "/sayHi") {
     // code here...
-    
-  }
-  else if (request.method === 'POST' && request.url === '/greeting') {
+  } else if (request.method === "POST" && request.url === "/greeting") {
     // accumulate the request body in a series of chunks
     // code here...
-    
-  }
-  else {
+  } else {
     // Handle 404 error: page not found
     // code here...
-    
   }
 }
 
-const server = http.createServer(doOnRequest)
+const server = http.createServer(doOnRequest);
 
 server.listen(3000);
